9ec971ce1e8c3534730f0e7756f7c7f229ff69b1
goodness <goodyc474@gmail.com>
2022-11-29 10:59:38 +0100

    add restart game

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 
+
+// variable declarations
 const messageEl = document.getElementById("message-el");
 const cardEl = document.getElementById("card-el");
 const sumEl = document.getElementById("sum-el");
 const startBtn = document.getElementById("start-game");
 const newBtn = document.getElementById("new-card");
+let isDisabled;
+const playerEl = document.getElementById("player")
 
-// let randomNum2 = Math.floor(Math.random()*10)
-// console.log(randomNum);
+// assigning random numbers to firstCard and secondCard 
 
 let firstCard = getRandomNum()
 let secondCard = getRandomNum();
 let cards = []
+let sum = 0
 
 let hasBlackJack = false
 let isAlive = true
-let sum = 0
+// let userName = prompt("enter your name")
 
 
+// create an object to store player name and chips
+
+let player = {
+    name:'goodness',
+    chips:231,
+
+}
+
+playerEl.textContent = player.name  + ": $" + player.chips
+
+// function declarations
+
+// get random number function
 function getRandomNum(){
     let randomNum = Math.floor(Math.random() * 13 ) + 1
 
@@ -35,7 +52,7 @@ function getRandomNum(){
 
 
 
-
+// start game function
 function startGame() {
 
     cards = [firstCard ,secondCard]
@@ -43,12 +60,14 @@ function startGame() {
     renderGame()
 }
 
+
+// Render game function 
 function renderGame(){
 
 cardEl.textContent =  "Card: "
 
 for(let i = 0; i<cards.length; i++ ){
-    cardEl.textContent +=cards[i] + " , "
+    cardEl.textContent += cards[i] + " , "
 } 
 sumEl.textContent = "sum: " + sum;
 
@@ -62,20 +81,26 @@ else if(sum === 21){
 }
    else{
 messageEl.textContent = "You're out of game";
+
 isAlive = false
+
    }
 }
 
-
+// new card function
 function newCard(){
 
     if(isAlive === true && hasBlackJack === false){
       console.log(isAlive, hasBlackJack);
         let card = getRandomNum()
         cards.push(card) 
-        sum+=card
+        sum += card
         renderGame()
     }
+    
+       
+   
+    
     
     
 }
